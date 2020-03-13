d2270c0adb55b4a23137d8441082ed269d9aa60e
Jirada Herbst <jirada.herbst@data2impact.com>
2020-03-13 16:10:47 +0100

    Trial code for the new pest grammar. The program is not fully working but it prints out the parse_result.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,23 @@ use calculator::calculate;
 use calculator::parse_formula;
 
 fn main() {
-    let formula = parse_formula::parse_string_to_formula(&"= 1773 + 1362");
-    println!("{:?}", formula);
-    let result = calculate::calculate_formula(formula);
-    println!("{:?}", result);
-    println!(
-        "Result from result_to_string is {}",
-        calculate::result_to_string(result)
-    );
-    let formula = parse_formula::parse_string_to_formula(&"= 123 - 456");
-    println!("{:?}", formula);
-    let result = calculate::calculate_formula(formula);
-    println!("{:?}", result);
-    println!(
-        "Result from result_to_string is {}",
-        calculate::result_to_string(result)
-    );
+    parse_formula::parse_string_to_formula(&"= 1773 + 1362");
+    parse_formula::parse_string_to_formula(&" =123-456 ");
+
+    // let formula = parse_formula::parse_string_to_formula(&"= 1773 + 1362");
+    // println!("{:?}", formula);
+    // let result = calculate::calculate_formula(formula);
+    // println!("{:?}", result);
+    // println!(
+    //     "Result from result_to_string is {}",
+    //     calculate::result_to_string(result)
+    // );
+    // let formula = parse_formula::parse_string_to_formula(&"= 123 - 456");
+    // println!("{:?}", formula);
+    // let result = calculate::calculate_formula(formula);
+    // println!("{:?}", result);
+    // println!(
+    //     "Result from result_to_string is {}",
+    //     calculate::result_to_string(result)
+    // );
 }
